063c93cff1a22628c07ede77aa4a20dee7c4ea26
nilhouses <nil.cases@gmail.com>
2025-08-19 18:30:45 +0200

    Display greetings to user

---
+++ local/greetings/index.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Main file to view greetings
+ *
+ * @package     local_greetings
+ * @copyright   2023 Your name <your@email>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+// Ignoro warning require_login();
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title(get_string('pluginname', 'local_greetings'));
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+echo $OUTPUT->header();
+
+// 1) Output greeting message using only PHP
+/*
+if (isloggedin()) {
+    echo '<h2>Greetings, ' . fullname($USER) . '</h2>';
+} else {
+    echo '<h2>Greetings, user</h2>';
+}
+*/
+
+// 2) Output greeting message using a mustache template
+if (isloggedin()) {
+    $usergreeting = 'Greetings, ' . fullname($USER);
+} else {
+    $usergreeting = 'Greetings, user';
+}
+
+$templatedata = ['usergreeting' => $usergreeting];
+
+echo $OUTPUT->render_from_template('local_greetings/greeting_message', $templatedata);
+
+echo $OUTPUT->footer();
