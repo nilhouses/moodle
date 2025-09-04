a3efc9b386df184afc033c1172f74209e6ce0313
Nil Casas Duatis <nil.cases@gmail.com>
2025-09-04 18:11:43 +0200

    Greetings migration  (local -> blocks)

---
+++ blocks/greetings/lang/en/block_greetings.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_greetings
+ * @category    string
+ * @copyright   2025 Nil Casas <nil.cases@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cannotaddgreeting'] = 'Sorry. You cannot post a greeting message.';
+$string['cannoteditmessage'] = 'You cannot edit this message';
+$string['editmessage'] = 'Edit message';
+$string['greetingloggedinuser'] = 'Greetings, {$a}.';
+$string['greetings'] = 'Greetings';
+$string['greetings:deleteanymessage'] = 'Delete any message on the Greetings wall';
+$string['greetings:deleteownmessage'] = 'Delete own message on the Greetings wall';
+$string['greetings:myaddinstance'] = 'Add a new Greetings block to Dashboard';
+$string['greetings:postmessages'] = 'Post a new message on the Greetings wall';
+$string['greetings:viewmessages'] = 'View messages on the Greetings wall';
+$string['greetinguser'] = 'Greetings, user.';
+$string['greetinguserau'] = 'Hello, {$a}.';
+$string['greetinguseres'] = 'Hola, {$a}.';
+$string['greetinguserfj'] = 'Bula, {$a}.';
+$string['greetingusernz'] = 'Kia Ora, {$a}.';
+$string['hello'] = 'Hello World';
+$string['messagecardbgcolor'] = 'Message card color';
+$string['messagecardbgcolor_help'] = 'Background color of the message card';
+$string['messagecardbgcolordesc'] = 'Background color of the message card';
+$string['nomessages'] = 'No messages posted yet.';
+$string['norecordfound'] = 'No record found!';
+$string['pluginname'] = 'Greetings';
+$string['postedby'] = 'Posted by {$a}.';
+$string['yourmessage'] = 'Your message';
+$string['yourmessagehint'] = 'Add your message here...';
