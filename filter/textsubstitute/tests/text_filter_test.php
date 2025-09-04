fc2e2bda7ea0b89ca0cc5957783274133bb4aee4
Nil Casas Duatis <nil.cases@gmail.com>
2025-09-04 10:03:26 +0200

    Added filter plugin with funcitonal settings page

---
+++ filter/textsubstitute/tests/text_filter_test.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace filter_textsubstitute;
+
+/**
+ * The text_filter test class.
+ *
+ * @package     filter_textsubstitute
+ * @category    test
+ * @copyright   2025 Nil Casas <nil.cases@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+final class text_filter_test extends \advanced_testcase {
+
+    // Write the tests here as public funcions.
+    // Please refer to {@link https://docs.moodle.org/dev/PHPUnit} for more details on PHPUnit tests in Moodle.
+
+    /**
+     * Dummy test.
+     *
+     * This is to be replaced by some actually useful test.
+     *
+     * @coversNothing
+     */
+    public function test_dummy(): void {
+        $this->assertTrue(false);
+    }
+}
